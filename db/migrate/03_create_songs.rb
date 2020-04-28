066d67d8ed91bf872f0d84d1d8e78f16d3cb636b
proiacm <proiacm@gmail.com>
2020-04-28 14:55:23 -0600

    Done.

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,12 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
+    def change
+        # create_join_table :artists, :genres, table_name: :songs, column_options: { null: true }
+        # add_column :songs, :name, :string
+        # change_column_null :songs, :name, false 
+        create_table :songs do |t|
+          t.string :name
+          t.integer :artist_id
+          t.integer :genre_id
+        end
+    end
 end
--- db/migrate/04_add_artist_to_songs.rb
+++ db/migrate/04_add_artist_to_songs.rb
@@ -1,2 +1,6 @@
 class AddArtistToSongs < ActiveRecord::Migration[4.2]
+
+    # def change 
+    #     add_column :songs, 
+    # end
 end
